053d8650e365b5d8528cc075a75d3d7398facae6
Rakib Hasan Bappy <rakibhasan15144@gmail.com>
2024-09-25 15:52:04 +0600

    feat:database client added

---
+++ server/services/database.js
@@ -0,0 +1,24 @@
+const { Client } = require('pg');
+require('dotenv').config();
+
+async function get_client() {
+    const client = new Client({
+        host: process.env.PG_HOST,
+        user: process.env.PG_USER,
+        password: process.env.PG_PASSWORD,
+        database: process.env.PG_DATABASE,
+        port: process.env.PG_PORT,
+        ssl: {
+            rejectUnauthorized: false, // Only use this in dev or if needed in your config
+        },
+      });
+    try {
+      await client.connect();
+      console.log("Connected to database");
+      return client;
+    } catch (err) {
+      console.log("Connection Error!", err);
+    }
+  }
+  
+  module.exports = get_client;
