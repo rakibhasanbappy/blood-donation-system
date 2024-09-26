8811b572dcd16e4914d2956ed13ffa3deb1ea0f1
Rakib Hasan Bappy <rakibhasan15144@gmail.com>
2024-09-26 13:18:32 +0600

    api: completed /create route in users route

--- server/middlewares/common/userValidator.js
+++ server/middlewares/common/userValidator.js
@@ -3,15 +3,59 @@ const { body, validationResult } = require('express-validator');
 const createError = require('http-errors');
 
 
-
+// register new user
 const addUserValidator = [
+
+    body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid Email Format")
+    .custom(async (value) => {
+      try {
+        const user = await User.findOne({ email: value });
+        if (user) {
+          throw new Error('A user already exists with this e-mail address');
+        }
+      } catch (err) {
+        throw createError(err.message);
+      }
+    }),
 
     body("name")
     .trim()
     .notEmpty()
     .withMessage("Name is required")
     .isAlpha("en-US", { ignore: "-" })
-    .withMessage("Name must be alphabetic")
+    .withMessage("Name must be alphabetic"),
+
+    body("password")
+    .isStrongPassword()
+    .withMessage("Password must be at least 8 characters long & should contain at least 1 lowercase, 1 uppercase, 1 number & 1 symbol"),
+
+    body("phone")
+    .isMobilePhone("bn-BD", {
+      strictMode: true,
+    })
+    .withMessage("Phone number must be a valid Bangladeshi mobile number"),
+
+    body("blood_group")
+    .trim()
+    .notEmpty()
+    .withMessage("Blood Group is required")
+    .isIn(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"])
+    .withMessage("Invalid Blood Group"),
+
+    body("is_available")
+    .isBoolean()
+    .withMessage("Availability status must be a boolean value"),
+
+    body("last_donated")
+    .optional()
+    .isDate()
+    .withMessage("Last donated date must be a valid date"),
+
 ];
 
 const addUserValidationHandler = function (req, res, next) {
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,14 +2,14 @@
 const router = require('express').Router();
 
 // internal dependencies
-const {addUserValidationHandler } = require('../middlewares/common/userValidator');
+const {addUserValidator, addUserValidationHandler } = require('../middlewares/common/userValidator');
 const { createUser } = require('../controllers/usersController');
 
 
 // routes
 // router.get('/', getAllUsers);
 // router.get('/:id', getUser);
-router.post('/create', addUserValidationHandler, createUser);
+router.post('/create', addUserValidator, addUserValidationHandler, createUser);
 // router.put('/:id', updateUser);
 // router.delete('/:id', deleteUser);
 
