44ef08baf75fcbac20358b22ae4dee882bcd96d3
Rakib Hasan Bappy <rakibhasan15144@gmail.com>
2024-09-27 15:37:18 +0600

    feat: login check middleware completed

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,14 +4,15 @@ const router = require('express').Router();
 // internal dependencies
 const {addUserValidator, addUserValidationHandler } = require('../middlewares/common/addUserValidator');
 const { loginValidator, loginValidationHandler } = require('../middlewares/common/loginValidator');
-const { createUser, getLogin } = require('../controllers/usersController');
+const { createUser, getLogin, getUserById } = require('../controllers/usersController');
+const { checkLogin, redirectLoggedIn } = require('../middlewares/common/checkLogin');
 
 
 // routes
 // router.get('/', getAllUsers);
-// router.get('/:id', getUser);
-router.post('/create', addUserValidator, addUserValidationHandler, createUser);
-router.post('/login', loginValidator, loginValidationHandler, getLogin);
+router.get('/:id', getUserById);
+router.post('/create', redirectLoggedIn, addUserValidator, addUserValidationHandler, createUser);
+router.post('/login', redirectLoggedIn, loginValidator, loginValidationHandler, getLogin);
 // router.put('/:id', updateUser);
 // router.delete('/:id', deleteUser);
 
